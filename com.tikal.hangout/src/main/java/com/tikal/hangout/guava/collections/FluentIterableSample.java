c22e0b0a41bf2a6eda400092e2ba02afcccbf4c0
shrek2000 <oren.kleks@gmail.xom>
2014-09-15 08:28:13 +0300

    fix typo

--- com.tikal.hangout/src/main/java/com/tikal/hangout/guava/collections/FluentIterableSample.java
+++ com.tikal.hangout/src/main/java/com/tikal/hangout/guava/collections/FluentIterableSample.java
@@ -3,13 +3,13 @@ package com.tikal.hangout.guava.collections;
 import static com.google.common.collect.FluentIterable.from;
 import static com.google.common.collect.ImmutableList.of;
 import static java.util.stream.Collectors.joining;
-
+import static com.google.common.base.Joiner.on;
 import com.google.common.collect.ImmutableList;
 public class FluentIterableSample {
 	
 	public static void main(String[] args) {
-		ImmutableList<String> cities = of("Tel Aviv", "Jerusalem","New Yourk", "Paris","Bagdad");
-		String result = from(cities).filter(city -> city.contains("e")).transform(city -> "'"+city + "'").limit(2).toString();
+		ImmutableList<String> cities = of("Tel Aviv", "Jerusalem","New York", "Paris","Bagdad");
+		String result = from(cities).filter(city -> city.contains("e")).transform(city -> "'"+city + "'").limit(2).join(on(" "));
 		System.out.println("FluentIterable result: " + result);
 		result = cities.stream().filter(city -> city.contains("e")).map( city ->"'"+city+"'").limit(2).collect(joining(" "));
 		System.out.println("Java 8 result: " + result);
